5b4ead3a9f393d504dbc0ea7e4639f5e98c65b78
„Valentin <valentinmueller@live.de>
2021-06-26 21:35:33 +0200

    Adding the patient pages with a first concept of the navigation bar. Adding routes to the app.py.

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("landing.html")
+
+@app.route("/patient")
+def patient_home():
+    return render_template("/patient/patient_vaccination_certificate.html")
+
+@app.route("/patient/impfwissen")
+def patient_impfwissen():
+    return render_template("/patient/patient_vaccination_knowledge.html")
+
+@app.route("/patient/kalender")
+def patient_kalender():
+    return render_template("/patient/patient_calendar.html")
+
+@app.route("/patient/scan")
+def patient_scan():
+    return render_template("/patient/patient_scan.html")
+
+@app.route("/patient/profil")
+def patient_profil():
+    return render_template("/patient/patient_profile.html")
 
 # Run application with debug console
 if __name__ == "__main__":
