d6b95dad4087f7e859fbb4e0e9cb8b516c1cd7aa
„Valentin <valentinmueller@live.de>
2021-07-02 14:04:32 +0200

    Definition of Models for database. Initialize connection to database.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-# Imports from Flask
+# 
+# Imports
+#
+
+import datetime
 from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize flask application
+
+# 
+# Initialization of Flask Application
+#
+
 app = Flask(__name__)
+
+
+#
+# DATABASE
+#
 
 # Location of the database 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Master123@localhost:5432/vaccination_database'
@@ -12,6 +25,65 @@ app.config['SQLALCHEMY_ECHO'] = True
 # Initializing database with SQLAlchemy
 db = SQLAlchemy(app)
 
+# Patient Model
+class Patient(db.Model):
+    # Primary Key for patient is the username
+    unique_patient_identifier = db.Column(db.String, primary_key = True)
+    
+    # Defining all required attributes
+    password = db.Column(db.String, nullable = False)
+    f_name = db.Column(db.String, nullable = False)
+    l_name = db.Column(db.String, nullable = False)
+    date_of_birth = db.Column(db.DateTime, nullable = False)
+    
+    # Defining relationship to proof_of_vaccination
+    proof_of_vaccination_identifier = db.relationship('Proof_of_vaccination', backref = 'patient') 
+
+# Issuer Model
+class Issuer(db.Model):
+    # Defining primary key
+    unique_issuer_identifier = db.Column(db.String, primary_key = True)
+    
+    # Defining all required attributes
+    password = db.Column(db.String, nullable = False)
+    f_name = db.Column(db.String, nullable = False)
+    l_name = db.Column(db.String, nullable = False)
+    date_of_birth = db.Column(db.DateTime, nullable = False)
+    
+    # Defining relationship to proof_of_vaccination
+    proof_of_vaccination_identifier = db.relationship('Proof_of_vaccination', backref = 'issuer') 
+
+# Proof_of_vaccination Model
+class Proof_of_vaccination(db.Model):
+    # Defining primary key
+    unique_certificate_identifier = db.Column(db.String, primary_key = True)
+    
+    # Defining all required attributes
+    date_of_vaccination = db.Column(db.DateTime, nullable = False)
+    vaccine_category = db.Column(db.String, nullable = False)
+    disease = db.Column(db.String, nullable = False)
+    vaccine = db.Column(db.String, nullable = False)
+    vaccine_marketing_authorization_holder = db.Column(db.String, nullable = False)
+    batch_number = db.Column(db.String, nullable = False)
+    issued_at = db.Column(db.DateTime, nullable = False)
+    
+    # Defining relationship to patient
+    unique_patient_identifier = db.Column(db.String, db.ForeignKey('patient.unique_patient_identifier'), nullable=False)
+    
+    # Defining relationship to issuer
+    unique_issuer_identifier = db.Column(db.String, db.ForeignKey('issuer.unique_issuer_identifier'), nullable=False)
+    
+    db.Column(db.String, nullable = False)
+
+# Create tables
+db.create_all()
+
+
+#
+# Routing
+#
+
+# Landing Page route
 @app.route("/")
 def home():
     return render_template("landing.html")
@@ -19,6 +91,13 @@ def home():
 # Patient routes
 @app.route("/patient")
 def patient_home():
+    
+    # EXAMPLE CODE: Creating new user
+    # birth_date = datetime.datetime(2020,5,17)
+    # new_patient = Patient(unique_patient_identifier = "12345678", password = "football4life", f_name="Valentin", l_name="Müller", date_of_birth=birth_date)
+    # db.session.add(new_patient)
+    # db.session.commit()
+    
     return render_template("/patient/patient_vaccination_certificate.html")
 
 @app.route("/patient/impfwissen")
@@ -51,7 +130,9 @@ def issuer_profil():
     return render_template("/issuer/issuer_profile.html")
 
 
-# Run application with debug console
+# 
+# Run application
+#
 if __name__ == "__main__":
     """ """
     app.run(debug=True, host="0.0.0.0", port=3000)
