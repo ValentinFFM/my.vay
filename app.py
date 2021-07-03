cf6477f64b6f703270b32503be2a4521a2f228d7
tristan2407 <tristan.schwarzer@psychonics.de>
2021-07-03 20:17:14 +0200

    First_working_QR_Code

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 # Imports from Flask
+import numpy as np
 from types import DynamicClassAttribute
 from flask import Flask, render_template, abort, url_for, redirect, flash
+from qrcode.main import QRCode
 from wtforms.meta import DefaultMeta
 from forms import ImpfnachweisForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
-import pyqrcode
+
 import qrcode
-#import png
+import pyqrcode
+import json
 #import cv2
+
+from PIL import Image
+from django.shortcuts import render
+import qrcode.image.svg
+from io import BytesIO
+from PIL import Image
+import io
+from io import StringIO
+from base64 import b64encode
 
 
 # Initialize flask application
@@ -15,7 +28,8 @@ app = Flask(__name__)
 # Bootstrap(app)
 app.config['SECRET_KEY'] = 'test'
 
-
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Master123@localhost:5432/vaccination_database'
+#app.config['SQLALCHEMY_ECHO'] = True
 
 @app.route("/")
 def home():
@@ -58,7 +72,11 @@ def issuer_profil():
 @app.route("/issuer/QR", methods =["GET", "POST"])   
 def issuer_create_qr():
     form = ImpfnachweisForm()
-    ## Clicking on the submit button is creating JSON-Object
+    qr = {}
+    img = []
+    file_object = io.BytesIO()
+
+## Clicking on the submit button is creating JSON-Object with input data
     if form.is_submitted():
         proof_of_vaccination= {}
         proof_of_vaccination['f_name']= form.f_name.data
@@ -70,25 +88,30 @@ def issuer_create_qr():
         proof_of_vaccination['batch_number'] = form.batch_number.data
         proof_of_vaccination['issued_at'] = form.issued_at.data
         proof_of_vaccination['certificate_issuer'] = form.certificate_issuer.data
-        ###Create QR Code with data of JSON-Object
-        qr = qrcode.make(proof_of_vaccination)
-        #qr.save('PrototypischerImpfnachweis.png')
-        ### display QR Code
-        qr.show()
-       #return redirect(url_for('show_qr', qr=qr))
-    return render_template("/issuer/issuer_create_qr.html", form=form)
+        
+        qr = QRCode(version=1, box_size=6,border=5)
+        qr.add_data(proof_of_vaccination)
+        qr.make()
+        #qr = qrcode.make(proof_of_vaccination)
+        img = qr.make_image (fill = 'black', back_color = 'white')
+        #img = np.array(img)
+        img.save(file_object,'PNG')
+        
 
+    return render_template("/issuer/issuer_create_qr.html", form=form, qr="data:image/png;base64,"+b64encode(file_object.getvalue()).decode('ascii'))
 
 
 
 @app.route("/login")
 def login():
-
     form = LoginForm()
     if form.validate_on_submit():   
-        if form.username.data == 'Arzt' and form.password.data =="Test":
+        if form.username.data == 'Patient1' and form.password.data =="Test":
             flash(f'Login erfolgreich für {form.username.data}', category = 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('patient_home'))
+        if form.username.data == 'Issuer1' and form.password.data =="Test":
+            return redirect(url_for('issuer_home'))
+            
          #else:
             # flash(f'Login fehlgeschlagen für {form.username.data}', category = 'danger')
     return render_template("/login/login.html", title = 'Login', form=form)
--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import Form
 from wtforms import TextField, StringField, DateTimeField, BooleanField, SubmitField, IntegerField, DateField, PasswordField
 from wtforms.fields.html5 import DateTimeLocalField
+from wtforms.validators import EqualTo, DataRequired, Length, ValidationError
+#from models import Patient
 
 class ImpfnachweisForm (Form):
     f_name = TextField("Vorname des Geimpften: ")
@@ -17,8 +19,27 @@ class ImpfnachweisForm (Form):
     generate_certificate =  SubmitField("Impfnachweis erstellen")
 
 class LoginForm(Form):
-    username = TextField("Nutzername")
-    password = PasswordField('Passwort (*)')
+    username = TextField("Nutzerkennung")
+    password = PasswordField('Passwort')
     remember = BooleanField('Passwort vergessen')
     submit = SubmitField('Login')
+
+
+class RegistrationForm(Form):
+    f_name = StringField('Vorname', validators=[DataRequired()])
+    l_name = StringField('Nachname', validators=[DataRequired()])
+    date_of_birth = DateField("Geburtsdatum des Geimpften: ",validators = [DataRequired()],format='%Y-%m-%d')
+    unique_patient_identifier = StringField("Nutzerkennung")
+    password = PasswordField('Passwort', validators=[DataRequired(), Length(min=8)])
+    confirmPassword = PasswordField('Passwort bestätigen', validators=[DataRequired(), EqualTo('password'), Length(min=8)])
+    submit = SubmitField('Registrieren')
+
+#validate unique_patient_identifier and check if it's already existing in patient database 
+    def validate_unique_patient_identifier(self, unique_patient_identifier):
+    
+        unique_patient_identifier = Patient.query.filter_by(unique_patient_identifier=unique_patient_identifier.data).first()
+        if unique_patient_identifier:
+            raise ValidationError('Nutzerkennung bereits vergeben')
+
+
 
